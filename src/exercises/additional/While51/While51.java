af4b19bf51c4c1057095291e592e533b0e051117
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2025-08-07 11:26:52 +0200

    add While51

---
+++ src/exercises/additional/While51/While51.java
@@ -0,0 +1,12 @@
+package exercises.additional.While51;
+
+public class While51 {
+    public static void main(String[] args) {
+        int i = 5;
+
+        while (i > 0) {
+            System.out.print(i + " ");
+            i--;
+        }
+    }
+}
